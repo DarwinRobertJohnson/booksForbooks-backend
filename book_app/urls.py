8c419907611ec9d87a240dc1a809ea2c34d575e2
Darwin <darwinrj852003@gmail.com>
2025-12-03 20:49:23 +0530

    Added Login Functionality

--- book_app/urls.py
+++ book_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
         path("", views.index, name = "index"),
-        path("recent-reads", views.recent_reads, name="recently read books")
+        path("recent-reads", views.recent_reads, name="recently read books"),
+        path("user-others/<str:user_name>",views.user_others, name="other-user-page"),
+        path("sign-in", views.sign_in, name="sign in"),
+        path('sign-out', views.sign_out, name="sign_out")
 ]
--- book_app/views.py
+++ book_app/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import BookEntry
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
+# Create your views here.
+@login_required(login_url='sign in')
 def index(request):
     if request.method == "GET":
         return render(request, "book_app/index.html")
     if request.method == "POST":
         book_entry = BookEntry(
-            user_name = request.POST["user_name"], 
+            user_name = request.user, 
             book_name = request.POST["book_name"], 
             author_name = request.POST["author_name"], 
             book_status = request.POST["book_status"]
@@ -16,7 +19,32 @@ def index(request):
         book_entry.save()
         return render(request, "book_app/index.html")
 
+def sign_in_post(request):
+    user = authenticate(username=request.POST["user_name"], password=request.POST["password"])
+    if user is not None:
+        login(request, user)
+        return redirect("index")
+    else:
+        return render(request, "book_app/sign_in.html")
+
+def sign_in(request):
+    if request.method=="POST":
+        return sign_in_post(request)
+    elif request.method=="GET":
+        return render(request, "book_app/sign_in.html")
+
+def sign_out(request):
+    logout(request)
+    return redirect("sign in")
+
+
 def recent_reads(request):
-    recently_read_books = {"book_list":list(BookEntry.objects.values('user_name','book_name','author_name'))}
-    print(recently_read_books)
-    return render(request, "book_app/recent_reads.html",recently_read_books)
+    if request.method =="GET":
+        recently_read_books = {"book_list":list(BookEntry.objects.values('user_name','book_name','author_name'))}
+        return render(request, "book_app/recent_reads.html",recently_read_books)
+        
+def user_others(request, user_name):
+    user_read_books = BookEntry.objects.filter(user_name= user_name)
+    currently_reading = BookEntry.objects.filter(book_status= True)
+    context={'user_name':user_name, 'book_list':user_read_books,'current_read':currently_reading}
+    return render(request, "book_app/user_others.html",context)
