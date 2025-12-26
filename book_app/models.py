9ec053a1139a2108f6303a4c821e52402106bf9b
Darwin <darwinrj852003@gmail.com>
2025-12-26 13:46:34 +0530

    add requirements

--- book_app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class BookEntry(models.Model):
-    user_name = models.CharField(default="Darwin D Dragon", max_length=30)
-    book_name = models.CharField(max_length=100)
-    author_name = models.CharField(null=True,max_length=30)
-    book_status = models.BooleanField(default=False)
--- book_app/serializers.py
+++
@@ -1,9 +0,0 @@
-
-from rest_framework import serializers
-from .models import BookEntry
-
-
-class BookEntrySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BookEntry
-        fields = "__all__"
